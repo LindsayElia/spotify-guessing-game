2f2fef4e152fe0634723ebc164b7f24c024d9eee
LindsayElia <lindsay.elia@gmail.com>
2015-06-23 15:38:00 -0700

    ajax request to Spotify API working, playing song on button submit working

--- public/js/playGame.js
+++ public/js/playGame.js
@@ -1,17 +1,19 @@
 $(function() {
 
+	// check that this file is connected to the page we're using it on
 	console.log("playGame.js file loaded");
 
+	// temporary list of songs
 	var songsArray = ["0eGsygTp906u18L0Oimnem", 
 						"5sra5UY6sD658OabHL3QtI", 
 						"6qOEjO2IUD7PjtpsXawq0d", 
-						"5ybJm6GczjQOgTqmJ0BomP"]
-
+						"5ybJm6GczjQOgTqmJ0BomP"];
 
 	var currentSong;
 
+	// when "start" button/form is clicked, make request and play new song
 	var $startGameForm = $("#startGameForm");
-	$startGameForm.on("submit", function(event){
+	$startGameForm.on("click", function(event){		// why is on.submit not working, but on.click works?
 		event.preventDefault();
 		console.log("#startGameForm submitted");
 
@@ -33,8 +35,13 @@ $(function() {
 			console.log(dataRcvd);
 			console.log("trying to capture just preview URL: " + dataRcvd.preview_url)
 			// put song preview URL in as the source for the audio element
-			
+			$audioPreviewUrl.attr("src", dataRcvd.preview_url);
 
+			// play the song
+			$audioPreviewUrl.on("canplay", function() {
+				$audioPreviewUrl[0].play();
+			});
+			
 		})
 		.fail(function(){
 			console.log("error with ajax request to Spotify API");
