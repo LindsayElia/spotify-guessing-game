1580dc19f490d21f467617fc405d151b4c6b9f9a
LindsayElia <lindsay.elia@gmail.com>
2015-06-23 16:18:25 -0700

    working on user guess input form

--- public/js/playGame.js
+++ public/js/playGame.js
@@ -6,10 +6,12 @@ $(function() {
 	// temporary list of songs
 	var songsArray = ["0eGsygTp906u18L0Oimnem", 
 						"5sra5UY6sD658OabHL3QtI", 
-						"6qOEjO2IUD7PjtpsXawq0d", 
-						"5ybJm6GczjQOgTqmJ0BomP"];
+						"6qOEjO2IUD7PjtpsXawq0d",
+						"5ybJm6GczjQOgTqmJ0BomP", // this one doesn't play anything because the previewUrl is "null"
+						"1BeY7Qw9d77wXOqABHpffT"];
 
 	var currentSong;
+	var counter = 0;
 
 	// when "start" button/form is clicked, make request and play new song
 	var $startGameForm = $("#startGameForm");
@@ -17,7 +19,7 @@ $(function() {
 		event.preventDefault();
 		console.log("#startGameForm submitted");
 
-		currentSong = songsArray[0];
+		currentSong = songsArray[counter];
 
 		var $audioPreviewUrl = $("#audioPreviewUrl");
 		console.log($audioPreviewUrl.attr("src"));
@@ -35,19 +37,27 @@ $(function() {
 			console.log(dataRcvd);
 			console.log("trying to capture just preview URL: " + dataRcvd.preview_url)
 			// put song preview URL in as the source for the audio element
+
+
+// !!!!
+// if dataRcvd.preview_url === "null" then tell the player to select a different song for their playlist
+// !!!!
+
+
 			$audioPreviewUrl.attr("src", dataRcvd.preview_url);
 
 			// play the song
 			$audioPreviewUrl.on("canplay", function() {
 				$audioPreviewUrl[0].play();
 			});
-			
+
 		})
 		.fail(function(){
 			console.log("error with ajax request to Spotify API");
 		});
 
-
+		// increase the counter
+		counter = counter + 1;
 
 	})
 
