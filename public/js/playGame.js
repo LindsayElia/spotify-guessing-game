3c13aa9d984f77ff05db4329bb2e19c80ee97506
LindsayElia <lindsay.elia@gmail.com>
2015-06-23 17:35:55 -0700

    music is playing and pausing when guess button is clicked, thanks Tim for help spotting my typo

--- public/js/playGame.js
+++ public/js/playGame.js
@@ -10,18 +10,30 @@ $(function() {
 						"5ybJm6GczjQOgTqmJ0BomP", // this one doesn't play anything because the previewUrl is "null"
 						"1BeY7Qw9d77wXOqABHpffT"];
 
+	// global variables
 	var currentSong;
 	var counter = 0;
+	var $startGameForm = $("#startGameForm");
+	var $audioPreviewUrl = $("#audioPreviewUrl");
+	var $divUserInputGuess = $("#divUserInputGuess");
+	var $inputGuessForm = $("#inputGuessForm");
+	var $divSongResults = $("#divSongResults");
+	var $divScore = $("#divScore");
+	var $pSongResults = $("#pSongResults");
+	var dataSpotify;
+
+	// hide parts of the page on inital page load
+	$divUserInputGuess.hide();
+	$divSongResults.hide();
+	$divScore.hide();
 
 	// when "start" button/form is clicked, make request and play new song
-	var $startGameForm = $("#startGameForm");
-	$startGameForm.on("click", function(event){		// why is on.submit not working, but on.click works?
+	$startGameForm.on("submit", function(event){		// why is on.submit not working, but on.click works?
 		event.preventDefault();
 		console.log("#startGameForm submitted");
 
 		currentSong = songsArray[counter];
 
-		var $audioPreviewUrl = $("#audioPreviewUrl");
 		console.log($audioPreviewUrl.attr("src"));
 		
 		//$audioPreviewUrl.attr("src", newURL goes here));
@@ -35,22 +47,24 @@ $(function() {
 		.done(function(dataRcvd, status){
 			console.log(status);
 			console.log(dataRcvd);
-			console.log("trying to capture just preview URL: " + dataRcvd.preview_url)
+			dataSpotify = dataRcvd;
+			console.log("trying to capture just preview URL: " + dataSpotify.preview_url)
 			// put song preview URL in as the source for the audio element
 
-
 // !!!!
-// if dataRcvd.preview_url === "null" then tell the player to select a different song for their playlist
+// if dataRcvd.preview_url (dataSpotify) === "null" then tell the player to select a different song for their playlist
 // !!!!
 
-
-			$audioPreviewUrl.attr("src", dataRcvd.preview_url);
+			$audioPreviewUrl.attr("src", dataSpotify.preview_url);
 
 			// play the song
 			$audioPreviewUrl.on("canplay", function() {
 				$audioPreviewUrl[0].play();
 			});
 
+			// hide the start button & show the guess form
+			$startGameForm.hide();
+			$divUserInputGuess.show();
 		})
 		.fail(function(){
 			console.log("error with ajax request to Spotify API");
@@ -59,7 +73,22 @@ $(function() {
 		// increase the counter
 		counter = counter + 1;
 
-	})
+	});
+
+	// on guess submit, do things...
+	$inputGuessForm.on("submit", function(event){
+		event.preventDefault();
+		console.log("#divUserInputGuess submitted");
+		$pSongResults.html("<li>" + dataSpotify.artists[0].name + "</li>");
+
+		// hide the guess button & show the results form
+		$divUserInputGuess.hide();
+		$divSongResults.show();
+
+		// stop music from playing
+		$audioPreviewUrl[0].pause();
+
+	});
 
 	// show/hide song info
 
