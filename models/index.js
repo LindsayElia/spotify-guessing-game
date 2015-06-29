74151419ac19e1fb0f8400ad723ab15f7cf5ed23
LindsayElia <lindsay.elia@gmail.com>
2015-06-29 16:12:48 -0700

    load process.env to models/index

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose");
+require('dotenv').load();
 
 // will need to add an OR option once I host this
 mongoose.connect("process.env.MONGOLAB_URI || mongodb://localhost/spotify_guessing_game_app");
